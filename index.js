5ca61b2f293516d544c1b8662292f38f188ebec2
Habibul Islam <habibulislam6862@gmail.com>
2022-03-19 20:19:24 +0600

    fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.dd4rb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dd4rb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
